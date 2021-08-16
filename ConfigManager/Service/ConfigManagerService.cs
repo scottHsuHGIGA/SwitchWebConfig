51876af2f63c84b133a5a02347a7dc65e2863ef2
scott <scott.hsu@hgiga.com>
2021-08-16 16:05:22 +0800

    second commit

---
+++ ConfigManager/Service/ConfigManagerService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace ConfigManager.Service
+{
+    public class ConfigManagerService
+    {
+        /// <summary>
+        /// 取得 AppSettings 介面
+        /// </summary>
+        public static readonly NameValueCollection AppSettings = GetConfigurationUsingAppSettings();
+
+        private static NameValueCollection GetConfigurationUsingAppSettings()
+        {
+            var configName = GetMachineAppSettings();
+            var postSetting = ConfigurationManager.GetSection(configName) as NameValueCollection;
+            return postSetting;
+        }
+
+        /// <summary>
+        /// 取得目前程式運行設備AppSettings
+        /// </summary>
+        /// <returns></returns>
+        private static string GetMachineAppSettings()
+        {
+            //預設設備為 MachineA
+            var defaultMachineName = "MachineA";
+            var group = "/appSettings";
+
+            //取得運行主機名稱
+            var machineName = Environment.MachineName.Trim().ToUpper();
+            string machineAName = "MachineA";//設備A名稱
+            string machineBName = "MachineB";//設備B名稱
+
+            if(machineAName.Trim().ToUpper().Contains(machineName))
+            {
+                machineName = machineAName;
+            }
+            else if (machineBName.Trim().ToUpper().Contains(machineName))
+            {
+                machineName = machineBName;
+            }
+            else
+            {
+                machineName = defaultMachineName;
+            }
+
+            return machineName + group;
+
+        }
+
+    }
+}
---
+++ SwitchWebConfig/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConfigManager.Service;
+
+namespace SwitchWebConfig.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["TestStringA"] =ConfigManagerService.AppSettings["TestStringA"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
